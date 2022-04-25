3147d568779101fc6add2ca6711aa34ba0fdc2cf
Felipe Santos <dev.felipesantos@gmail.com>
2022-04-25 20:49:59 -0300

    feat: create user controller

---
+++ src/controllers/users/CreateUserController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express'
+import { hash } from 'bcryptjs'
+import { prismaClient } from '../../database/prismaClient'
+
+export class CreateUserController {
+  async handle(request: Request, response: Response) {
+    const { email, password } = request.body
+
+    const encryptedPassword = await hash(password, 8)
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email
+      }
+    })
+
+    if (userAlreadyExists) {
+      response.status(200).json({ message: 'Usuário já existente!' })
+    } else {
+      try {
+        await prismaClient.user.create({
+          data: {
+            email,
+            password: encryptedPassword
+          }
+        })
+        response.status(201).json({ message: `Usuário(a) cadastrado(a)!` })
+      } catch (error) {
+        response.status(200).json({ message: error.message })
+      }
+    }
+  }
+}
