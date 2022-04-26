407c808cf888fecc4e5057abf2ad066cb216df01
Felipe Santos <dev.felipesantos@gmail.com>
2022-04-26 14:23:34 -0300

    feat: create category service

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,11 +1,3 @@
-import { prismaClient } from '../database/prismaClient'
+// import { prismaClient } from '../database/prismaClient'
 
-export class CategoriesRepository {
-  async findByName(name: string) {
-    const findCategory = await prismaClient.category.findFirst({
-      where: { name }
-    })
-
-    return findCategory || null
-  }
-}
+export class CategoriesRepository {}
