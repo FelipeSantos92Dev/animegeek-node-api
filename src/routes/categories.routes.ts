7f9be46bb71d4ccb1e2020f239725d6da3bcfd7b
Felipe Santos <dev.felipesantos@gmail.com>
2022-04-27 11:18:48 -0300

    feat: ensure user is administrator on routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 import CreateCategoryController from '../controllers/categories/CreateCategoryController'
 import ListAllCategoriesController from '../controllers/categories/ListAllCategoriesController'
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import ensureAccessByRole from '../middlewares/ensureAccessByRole'
 
 const categoriesRouter = Router()
 
 categoriesRouter.use(new ensureAuthenticated().handle)
+categoriesRouter.use(new ensureAccessByRole().handle)
 
 const createCategory = new CreateCategoryController()
 const listCategories = new ListAllCategoriesController()
--- src/routes/roles.routes.ts
+++ src/routes/roles.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import CreateRoleController from '../controllers/roles/CreateRoleController'
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import ensureAccessByRole from '../middlewares/ensureAccessByRole'
 
 const rolesRouter = Router()
 
 rolesRouter.use(new ensureAuthenticated().handle)
+rolesRouter.use(new ensureAccessByRole().handle)
 
 const createRole = new CreateRoleController()
 
