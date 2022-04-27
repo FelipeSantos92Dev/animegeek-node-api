c689b6efa96409beb124bd7cf46866c471867495
Felipe Santos <dev.felipesantos@gmail.com>
2022-04-26 21:30:29 -0300

    feat: create profiles migration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express'
 
 import { authenticateRouter } from './authenticate.routes'
-import { categoriesRouter } from './categories.routes'
 import { usersRouter } from './users.routes'
+import { rolesRouter } from './roles.routes'
+import { categoriesRouter } from './categories.routes'
 
 const router = Router()
 
 router.use(authenticateRouter)
-router.use('/categories', categoriesRouter)
 router.use('/users', usersRouter)
+router.use('/roles', rolesRouter)
+router.use('/categories', categoriesRouter)
 
 export { router }
