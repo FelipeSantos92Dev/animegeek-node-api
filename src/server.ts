287f9c2dec073b351f4b3a17c127713d65847ae6
Felipe Santos <dev.felipesantos@gmail.com>
2022-05-03 12:03:12 -0300

    refactor: cors header

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,11 @@ import AppError from './errors/AppError'
 
 const app = express()
 app.use(express.json())
-const corsOptions = {
-  origin: '*',
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200
-}
-app.use(cors(corsOptions))
+app.use((request: Request, response: Response, next: NextFunction) => {
+  response.header('Access-Control-Allow-Origin', '*')
+  app.use(cors())
+  next()
+})
 app.use(router)
 
 app.use(
