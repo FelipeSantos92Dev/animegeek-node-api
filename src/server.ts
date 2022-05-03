ae71ffaa3b1eedfad1437aab6154f7404d09d8e9
Felipe Santos <dev.felipesantos@gmail.com>
2022-05-03 12:06:30 -0300

    refactor: cors headers

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,12 @@ const app = express()
 app.use(express.json())
 app.use((request: Request, response: Response, next: NextFunction) => {
   response.header('Access-Control-Allow-Origin', '*')
+  response.header(
+    'Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  )
+  response.header(
+    'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'
+  )
   app.use(cors())
   next()
 })
