fd71fe7c412e4f9d38685b1fdb70e84a6f92cb2a
Felipe Santos <dev.felipesantos@gmail.com>
2022-05-13 19:04:34 -0300

    refactor: create cart controller

--- src/controllers/auth/GenerateRefreshToken.ts
+++ src/controllers/auth/GenerateRefreshToken.ts
@@ -3,7 +3,7 @@ import { prismaClient } from '../../database/prismaClient'
 
 export default class GenerateRefreshToken {
   async execute(userId: string) {
-    const expiresIn = dayjs().add(45, 'second').unix()
+    const expiresIn = dayjs().add(1, 'day').unix()
     const generateRefreshToken = await prismaClient.refreshToken.create({
       data: {
         user_id: userId,
--- src/controllers/carts/CreateCartController.ts
+++ src/controllers/carts/CreateCartController.ts
@@ -1,22 +1,66 @@
 import { Request, Response } from 'express'
 import { prismaClient } from '../../database/prismaClient'
+import AppError from '../../errors/AppError'
 
 export default class CreateCartController {
   async handle(request: Request, response: Response) {
-    const { price, quantity } = request.body
+    const { cartOne, cartTwo, cartThree, totalCost } = request.body.cart
+    const { id } = request.user
 
-    let count = quantity
+    const user = await prismaClient.user.findFirst({
+      where: {
+        id
+      }
+    })
 
-    while (count > 0) {
-      await prismaClient.cart.create({
-        data: {
-          price: price * 100
-        }
-      })
-
-      count = count - 1
+    if (!user) {
+      throw new AppError('Usuário não encontrado', 404)
     }
 
-    return response.json()
+    const cart = await prismaClient.cart.create({
+      data: {
+        price: totalCost,
+        user_id: user.id,
+        items: { cartOne, cartTwo, cartThree }
+      }
+    })
+
+    let countOne = cartOne
+
+    while (countOne > 0) {
+      await prismaClient.ticket.create({
+        data: {
+          cart_id: cart.id,
+          category_id: '35aeb1ac-edea-4481-841d-8be5dc61a0f7'
+        }
+      })
+      countOne = countOne - 1
+    }
+
+    let countTwo = cartTwo
+
+    while (countTwo > 0) {
+      await prismaClient.ticket.create({
+        data: {
+          cart_id: cart.id,
+          category_id: '8c01e317-6e7b-499c-9555-f103132606c1'
+        }
+      })
+      countTwo = countTwo - 1
+    }
+
+    let countThree = cartThree
+
+    while (countThree > 0) {
+      await prismaClient.ticket.create({
+        data: {
+          cart_id: cart.id,
+          category_id: 'dc575398-c268-4e73-b891-aadba875b861'
+        }
+      })
+      countThree = countThree - 1
+    }
+
+    return response.send()
   }
 }
