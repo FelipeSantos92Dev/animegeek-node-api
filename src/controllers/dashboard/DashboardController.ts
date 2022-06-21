d1205aaf447f26367bf3d00f4447bbc4003e43e8
Felipe Santos <dev.felipesantos@gmail.com>
2022-06-21 19:12:57 -0300

    refactor: tickets counter

--- src/controllers/dashboard/DashboardController.ts
+++ src/controllers/dashboard/DashboardController.ts
@@ -3,29 +3,57 @@ import { prismaClient } from '../../database/prismaClient'
 
 export default class DashboardController {
   async handle(request: Request, response: Response) {
-    const totalCombo = await prismaClient.ticket.count({
+    const totalComboCredit = await prismaClient.ticket.count({
       where: {
         category_id: '72970a23-06a3-4778-aead-b75a4f38fd4a',
         status: 'approved',
-        type: 'online' || 'onlinePix'
+        type: 'online'
       }
     })
 
-    const totalSab = await prismaClient.ticket.count({
+    const totalSabCredit = await prismaClient.ticket.count({
       where: {
         category_id: 'fb6c5dd6-66e6-472f-b484-aac9fe6baf64',
         status: 'approved',
-        type: 'online' || 'onlinePix'
+        type: 'online'
       }
     })
 
-    const totalDom = await prismaClient.ticket.count({
+    const totalDomCredit = await prismaClient.ticket.count({
       where: {
         category_id: 'd8356f00-c334-4a75-92d2-503e90e3d6ba',
         status: 'approved',
         type: 'online'
       }
     })
+
+    const totalComboPix = await prismaClient.ticket.count({
+      where: {
+        category_id: '72970a23-06a3-4778-aead-b75a4f38fd4a',
+        status: 'approved',
+        type: 'onlinePix'
+      }
+    })
+
+    const totalSabPix = await prismaClient.ticket.count({
+      where: {
+        category_id: 'fb6c5dd6-66e6-472f-b484-aac9fe6baf64',
+        status: 'approved',
+        type: 'onlinePix'
+      }
+    })
+
+    const totalDomPix = await prismaClient.ticket.count({
+      where: {
+        category_id: 'd8356f00-c334-4a75-92d2-503e90e3d6ba',
+        status: 'approved',
+        type: 'onlinePix'
+      }
+    })
+
+    const totalSab = totalSabCredit + totalSabPix
+    const totalDom = totalDomCredit + totalDomPix
+    const totalCombo = totalComboCredit + totalComboPix
 
     return response.json({
       totalSab,
