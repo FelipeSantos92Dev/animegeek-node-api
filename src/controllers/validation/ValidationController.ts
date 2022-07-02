cea14eb01f6f2a6b086720f2ca8a42c44215128e
Felipe Santos <dev.felipesantos@gmail.com>
2022-07-02 17:36:58 -0300

    feat: ticket increase validations backend

--- src/controllers/validation/ValidationController.ts
+++ src/controllers/validation/ValidationController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { prismaClient } from '../../database/prismaClient'
-import AppError from '../../errors/AppError'
+// import AppError from '../../errors/AppError'
 
 export default class ValidationController {
   async handle(request: Request, response: Response) {
