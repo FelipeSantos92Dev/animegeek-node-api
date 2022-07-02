033ab2ecc153314b1b992e7d37908203fc8f552d
Felipe Santos <dev.felipesantos@gmail.com>
2022-07-02 16:28:40 -0300

    feat: ticket increase validations

--- src/controllers/validation/ValidationController.ts
+++ src/controllers/validation/ValidationController.ts
@@ -5,6 +5,7 @@ import AppError from '../../errors/AppError'
 export default class ValidationController {
   async handle(request: Request, response: Response) {
     const { ticketId } = request.body
+    console.log(ticketId)
 
     const ticket = await prismaClient.ticket.findFirst({
       where: {
@@ -17,15 +18,61 @@ export default class ValidationController {
     }
 
     if (ticket.category_id === 'fb6c5dd6-66e6-472f-b484-aac9fe6baf64') {
-      return response.status(200).json({ message: 'Ingresso de Sábado!' })
+      if (ticket.validations > 0) {
+        return response
+          .status(200)
+          .json({ message: 'Ingresso Válido Para Sábado Já Utilizado!' })
+      } else {
+        await prismaClient.ticket.update({
+          where: {
+            id: ticketId
+          },
+          data: {
+            validations: 1
+          }
+        })
+        return response
+          .status(200)
+          .json({ message: 'Ingresso Válido Para Sábado!' })
+      }
     }
 
     if (ticket.category_id === 'd8356f00-c334-4a75-92d2-503e90e3d6ba') {
-      return response.status(200).json({ message: 'Ingresso de Domingo!' })
+      if (ticket.validations > 0) {
+        return response
+          .status(200)
+          .json({ message: 'Ingresso Válido Para Domingo Já Utilizado!' })
+      } else {
+        await prismaClient.ticket.update({
+          where: {
+            id: ticketId
+          },
+          data: {
+            validations: 1
+          }
+        })
+        return response
+          .status(200)
+          .json({ message: 'Ingresso Válido Para Domingo!' })
+      }
     }
 
     if (ticket.category_id === '72970a23-06a3-4778-aead-b75a4f38fd4a') {
-      return response.status(200).json({ message: 'Combo!' })
+      if (ticket.validations > 1) {
+        return response
+          .status(200)
+          .json({ message: 'Ingresso Combo Já Utilizado!' })
+      } else {
+        await prismaClient.ticket.update({
+          where: {
+            id: ticketId
+          },
+          data: {
+            validations: 1
+          }
+        })
+        return response.status(200).json({ message: 'Ingresso Combo Válido!' })
+      }
     }
 
     // return response.status(200).json({ ticket })
